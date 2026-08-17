ba35025f20d9d28e0df457cf1ffe8236a244f4c3
engled83 <53045928+engled83@users.noreply.github.com>
2026-08-17 09:17:23 -0400

    Update guests.js

--- js/guests.js
+++ js/guests.js
@@ -2,6 +2,7 @@ const guests = {
     "TESTCODE": {
         name: "Test Guest",
         date: "August 31, 2026",
-        time: "7:00 PM ET"
+        time: "7:00 PM ET",
+        datetime: "2026-08-31T19:00:00-04:00"
     }
 };

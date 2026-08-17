855579754c038429dffdcf9fa6656cfa727681b2
engled83 <53045928+engled83@users.noreply.github.com>
2026-08-17 14:46:25 -0400

    Add podcast guest Cody Ikerd

--- js/guests.js
+++ js/guests.js
@@ -4,5 +4,12 @@ const guests = {
         date: "August 31, 2026",
         time: "7:00 PM ET",
         datetime: "2026-08-31T19:00:00-04:00"
+    },
+
+    "32ZK": {
+        name: "Cody Ikerd",
+        date: "Wednesday, August 19, 2026",
+        time: "7:00 PM ET",
+        datetime: "2026-08-19T19:00:00-04:00"
     }
 };

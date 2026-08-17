eeac95488736571f30ee822122e3f464ad13a533
engled83 <53045928+engled83@users.noreply.github.com>
2026-08-17 15:02:56 -0400

    Remove podcast guest Test Guest

--- js/guests.js
+++ js/guests.js
@@ -1,11 +1,4 @@
 const guests = {
-    "TESTCODE": {
-        name: "Test Guest",
-        date: "August 31, 2026",
-        time: "7:00 PM ET",
-        datetime: "2026-08-31T19:00:00-04:00"
-    },
-
     "32ZK": {
         name: "Cody Ikerd",
         date: "Wednesday, August 19, 2026",

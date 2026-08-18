638f47c223ed7f567257c3273bff0d2e21894014
engled83 <53045928+engled83@users.noreply.github.com>
2026-08-18 09:12:49 -0400

    Add podcast guest Jackson Hymel

--- js/guests.js
+++ js/guests.js
@@ -4,5 +4,12 @@ const guests = {
         date: "Wednesday, August 19, 2026",
         time: "7:00 PM ET",
         datetime: "2026-08-19T19:00:00-04:00"
+    },
+
+    "2XF4": {
+        name: "Jackson Hymel",
+        date: "Monday, August 31, 2026",
+        time: "8:00 PM ET",
+        datetime: "2026-08-31T20:00:00-04:00"
     }
 };

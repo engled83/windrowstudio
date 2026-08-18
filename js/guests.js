a906159c5fffc9370f0c9bb4ec323ff23b0c4106
engled83 <53045928+engled83@users.noreply.github.com>
2026-08-18 10:11:44 -0400

    Add podcast guest Dane

--- js/guests.js
+++ js/guests.js
@@ -11,5 +11,12 @@ const guests = {
         date: "Monday, August 31, 2026",
         time: "8:00 PM ET",
         datetime: "2026-08-31T20:00:00-04:00"
+    },
+
+    "NT7V": {
+        name: "Dane",
+        date: "Tuesday, August 18, 2026",
+        time: "10:15 AM ET",
+        datetime: "2026-08-18T10:15:00-04:00"
     }
 };

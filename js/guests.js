e3631150a06784bb4d0fa5a8191641559c5af452
engled83 <53045928+engled83@users.noreply.github.com>
2026-08-18 11:59:25 -0400

    Add podcast guest Dane

--- js/guests.js
+++ js/guests.js
@@ -11,5 +11,12 @@ const guests = {
         date: "Monday, August 31, 2026",
         time: "8:00 PM ET",
         datetime: "2026-08-31T20:00:00-04:00"
+    },
+
+    "YX27": {
+        name: "Dane",
+        date: "Tuesday, August 18, 2026",
+        time: "12:10 PM ET",
+        datetime: "2026-08-18T12:10:00-04:00"
     }
 };
